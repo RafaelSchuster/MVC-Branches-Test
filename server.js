e14964cb2c4029da81c57f1ae0be97354f340355
RAFAEL SCHUSTER <rsc1335@gmail.com>
2021-01-29 16:21:13 +0200

    fixing

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const { test } = require('media-typer');
 const app = express();
 const mainRouter = require('./routers/mainRouter');
 const partyRouter = require('./routers/PartyRouters');
-const testRouter =require('./routers/TestRouter');
+const testRouter = require('./routers/TestRouter')
 
 app.use(mainRouter);
 app.use(partyRouter);
+app.use(testRouter);
 
 app.listen(5000, () => {
     console.log('Running on 5000');
